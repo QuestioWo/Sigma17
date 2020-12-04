207b5e48ff8686779b59406b48d1a5b7cb8f5625
Jim Carty <cartyjim1@gmail.com>
2020-12-04 00:49:29 +0000

    added wasm builds and basic function

---
+++ src/components/utils/EmulatorImport.js
@@ -0,0 +1,13 @@
+/*
+  Copyright Jim Carty © 2020
+
+  This file is subject to the terms and conditions defined in file 'LICENSE.txt', which is part of this source code package.
+*/
+
+import { instantiateStreaming } from 'assemblyscript/lib/loader';
+
+export default instantiateStreaming(
+  fetch(`${process.env.PUBLIC_URL}/wasm/Emulator.wasm`)
+).then(emu => {
+  return emu.exports;
+});
